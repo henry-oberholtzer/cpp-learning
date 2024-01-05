1b191bfebe357a9f7c389c3889e3301b0a6573d8
Henry Oberholtzer <henryoberholtzer@gmail.com>
2024-01-04 20:53:33 -0800

    c++ practicegit add . :)

--- helloworld/helloworld.cpp
+++ helloworld/helloworld.cpp
@@ -2,5 +2,23 @@
 
 int main()
 {
-  std::cout << "Hello World" << std::endl;
+  int width;
+  width = 5;
+  std::cout << width;
+  width = 7;
+  std::cout << width;
 }
+
+// Initialization
+int a;
+int b = 5;
+int c(6);
+int d{7};
+int e = {8};
+int f{};
+
+// Value initilization
+
+int width{};
+int x{0};
+int c{4}, d{88};
